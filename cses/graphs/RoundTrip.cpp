71a05645564760940a87737be51470cc94979601
PedroH2003 <pedrohenriquecandidosilva14@gmail.com>
2025-05-06 20:26:55 -0300

    Novos exercicios

--- cses/graphs/RoundTrip.cpp
+++ cses/graphs/RoundTrip.cpp
@@ -2,59 +2,66 @@
 
 using namespace std;
 
-const int MAX = 1e5+10;
+const int MAX = 1e5+10, INF = 0x3f3f3f3f;
+int n,m, ini,fim;
+bool ciclo = false;
 vector<vector<int>> g(MAX);
 vector<int> vis(MAX);
 vector<int> pai(MAX);
-vector<int> ans;
-
-void ciclo(int u, int s){
-    ans.push_back(u);
-    for(int i=s; i != u; i = pai[i]){
-        ans.push_back(i);
-    }
-    ans.push_back(u);
-}
 
 void dfs(int s){
     vis[s] = 1;
 
     for(auto u: g[s]){
         if(!vis[u]){
-            vis[u] = 1;
             pai[u] = s;
             dfs(u);
         }
-        else if(pai[s] != u and ans.size() == 0){
-            ciclo(u, s);
+        else{
+            if(pai[s] != u and !ciclo){
+                ini = s; fim = u;
+                ciclo = true;
+            }
         }
     }
+    
+}
+
+vector<int> path(int v){
+    vector<int> ans;
+    ans.push_back(fim+1);
+    while(v != fim){
+        ans.push_back(v+1);
+        v = pai[v];
+    }
+    ans.push_back(fim+1);
+    return ans;
 }
 
 int main(){
 
-    int n,m; cin >> n >> m;
+    cin >> n >> m;
     for(int i=0; i<m; i++){
-        int x,y; cin >> x >> y; x--,y--;
-
-        g[x].push_back(y);
-        g[y].push_back(x);
+        int a,b; cin >> a >> b; a--,b--;
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
 
-    for(int i=0; i<n and ans.size() == 0; i++){
+    vector<int> ans;
+    for(int i=0; i<n; i++){
         if(!vis[i]){
-            pai[i] = i;
             dfs(i);
+            if(ciclo){
+                ans = path(ini);
+                break;
+            }
         }
-        
     }
-    
-    if(ans.size() == 0) cout << "IMPOSSIBLE" << endl;
+
+    if(!ciclo) cout << "IMPOSSIBLE" << endl;
     else{
         cout << ans.size() << endl;
-        for(auto u: ans){
-            cout << u+1 << " ";
-        }
+        for(auto u: ans) cout << u << " ";
         cout << endl;
     }
 
